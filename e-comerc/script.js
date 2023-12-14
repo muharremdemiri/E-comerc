94dd0f16dfe75e633624fd29e63ef6464a57d980
muharremdemiri <92821212+muharremdemiri@users.noreply.github.com>
2023-12-15 00:28:30 +0100

    Add files via upload

---
+++ e-comerc/script.js
@@ -0,0 +1,105 @@
+
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+cartIcon.addEventListener('click', function () {
+    cart.classList.add("active");
+});
+
+closeCart.addEventListener('click', function () {
+    cart.classList.remove("active");
+});
+
+if (document.readyState == 'loading') {
+    document.addEventListener("DOMContentLoaded", ready);
+} else {
+    ready();
+}
+
+function ready() {
+    let removeCartButtons = document.getElementsByClassName('cart-remove');
+    for (let i = 0; i < removeCartButtons.length; i++) {
+        let button = removeCartButtons[i];
+        button.addEventListener("click", removeCartItem);
+    }
+
+    let quantityInputs = document.getElementsByClassName('cart-quantity');
+    for (let i = 0; i < quantityInputs.length; i++) {
+        let input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged);
+    }
+
+    let addCart = document.getElementsByClassName('add-cart');
+    for (let i = 0; i < addCart.length; i++) {
+        let button = addCart[i];
+        button.addEventListener('click', addCartClicked);
+    }
+}
+
+function removeCartItem(event) {
+    let buttonClicked = event.target;
+    buttonClicked.parentNode.remove();
+    updateTotal(); 
+}
+
+function quantityChanged(event) {
+    let input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updateTotal();
+}
+
+function addCartClicked(event) {
+    let button = event.target;
+    let shopProducts = button.parentElement;
+    let title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+    let price = shopProducts.getElementsByClassName("price")[0].innerText;
+    let productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(title, price, productImg);
+    updateTotal(); 
+}
+
+function addProductToCart(title, price, productImg) {
+    let cartShopBox = document.createElement("div");
+    cartShopBox.classList.add("cart-box");
+    let cartItems = document.querySelector('.cart-content'); 
+    let cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
+    for (let i = 0; i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText == title) {
+            alert("You have already added this item to the cart");
+            return;
+        }
+    }
+
+    let cartBoxContent = `<img src="${productImg}" alt="" class="cart-img">
+                          <div class="detail-box">
+                            <div class="cart-product-title">${title}</div>
+                            <div class="cart-price">${price}</div>
+                            <input type="number" value="1" class="cart-quantity">
+                          </div>
+                          <i class='bx bxs-trash-alt cart-remove'></i>`;
+
+    cartShopBox.innerHTML = cartBoxContent;
+    cartItems.appendChild(cartShopBox); 
+    cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem); 
+    cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged);
+}
+
+function updateTotal() {
+    let cartContent = document.querySelector('.cart-content'); 
+    let cartBoxes = cartContent.getElementsByClassName('cart-box');
+    let total = 0;
+    for (let i = 0; i < cartBoxes.length; i++) {
+        let cartBox = cartBoxes[i];
+        let priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        let quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        let price = parseFloat(priceElement.innerText.replace("$", ""));
+        let quantity = quantityElement.value;
+        total = total + price * quantity;
+        total = Math.round(total * 100) / 100; 
+    }
+
+    document.querySelector('.total-price').innerText = '$' + total; 
+}
